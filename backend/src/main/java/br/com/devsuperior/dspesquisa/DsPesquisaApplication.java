06931c5bad121850150dcf6a56525069881f41ab
Anderson Barreto <andersonsb09@hotmail.com>
2020-09-14 10:52:14 -0300

    Project created

---
+++ backend/src/main/java/br/com/devsuperior/dspesquisa/DsPesquisaApplication.java
@@ -0,0 +1,13 @@
+package br.com.devsuperior.dspesquisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsPesquisaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsPesquisaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/devsuperior/dspesquisa/DsPesquisaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devsuperior.dspesquisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsPesquisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
