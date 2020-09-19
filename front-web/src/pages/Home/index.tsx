db83c0f7b6173660f36def91834be20519e16d90
Anderson Barreto <andersonsb09@hotmail.com>
2020-09-19 11:48:59 -0300

    Criando navegação com rotas

--- front-web/src/pages/Home/index.tsx
+++ front-web/src/pages/Home/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ReactComponent as ArrowIcon } from '../../assets/arrow.svg';
 import { ReactComponent as GamerImage } from '../../assets/gamer.svg';
+import {Link} from 'react-router-dom';
 import './styles.css';
 
 const Home =() => (
@@ -13,14 +14,16 @@ const Home =() => (
                 Clique no botão abaixo e saiba quais são os 
                 jogos que os gamers estão escolhendo!
             </h3>
-            <div className="home-actions">
-                <button className="home-btn">
-                    QUERO SABER QUAIS SÃO
-                </button>
-                <div className="home-btn-icon">
-                    <ArrowIcon/>
+            <Link to="/records">
+                <div className="home-actions">
+                    <button className="home-btn">
+                        QUERO SABER QUAIS SÃO
+                    </button>
+                    <div className="home-btn-icon">
+                        <ArrowIcon/>
+                    </div>
                 </div>
-            </div>
+            </Link>
         </div>
         <div >
             <GamerImage className="home-image"/>
---
+++ front-web/src/pages/Records/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Records = () => (
+    <h1>
+        Records
+    </h1>
+);
+
+export default Records;
