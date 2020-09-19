b3d977074e2416a5bf2c27b9da879f652f2e763a
Anderson Barreto <andersonsb09@hotmail.com>
2020-09-19 09:52:59 -0300

    adding component header

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
-    <h1>Hello</h1>
+    <div>
+      <Header/>
+    </div>
   );
 }
 
 export default App;
+ 
